30ebcb7a6ac656b9814a9a41323577dabeb3f17f
ssathyanaray2 <ssathyanaray2@binghmaton.edu>
2025-05-15 17:16:51 -0400

    add databse file to create a connection pool

---
+++ database/databaseConfig.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT,
+  });
+
+  export default pool;
